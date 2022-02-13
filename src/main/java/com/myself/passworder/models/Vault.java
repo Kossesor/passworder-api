9451d5de2cb0ed6602a077f40271a5e92a1aafbb
FiNaLiZee <sd103e@mail.ru>
2022-02-14 01:28:31 +0300

    Create vault table

--- src/main/java/com/myself/passworder/models/Vault.java
+++ src/main/java/com/myself/passworder/models/Vault.java
@@ -1,35 +1,34 @@
-package com.myself.passworder.models;//package me.remember.passworder.models;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Builder;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//import javax.persistence.*;
-//
-//@Data
-//@Entity
-//@Builder
-//@Table(name = "vault")
-//@NoArgsConstructor
-//@AllArgsConstructor
-//public class Vault {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long user_id;
-//
-//    private String title;
-//    private String description;
-//
-//    @Column(columnDefinition = "tinyint")
-//    private int type;
-//
-//    private String username;
-//    private String mail;
-//    private String password;
-//
-//    private String secretQuestion; // ????
-//    private String secretQAnswer;  // ????
-//
-//    private String customInputs;   // HashMap<String, String> in JSON
-//}
+package com.myself.passworder.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.sql.Blob;
+
+@Data
+@Entity
+@Builder
+@Table(name = "vault")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Vault {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(min = 3, max = 50)
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    @Lob
+    @NotNull
+    @Size(min = 3, max = 50)
+    @Column(length = 50, nullable = false)
+    private Blob data;
+}

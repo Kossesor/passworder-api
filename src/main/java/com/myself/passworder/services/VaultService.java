c9ad846c317f5112cb8f267334b50e8ad13079d4
FiNaLiZee <sd103e@mail.ru>
2022-02-20 20:02:26 +0300

    From blob to text in controller and service methods

--- src/main/java/com/myself/passworder/services/VaultService.java
+++ src/main/java/com/myself/passworder/services/VaultService.java
@@ -4,12 +4,16 @@ import com.myself.passworder.repositories.VaultRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.sql.Blob;
+
 @Service
 @RequiredArgsConstructor
 public class VaultService {
     private final VaultRepository vaultRepository;
 
-    public final String getVaultDataById(Long id) {
-        return String.valueOf(vaultRepository.getVaultById(id).getData());
+    public final @NotNull @Size(min = 3, max = 50) Blob getVaultDataById(Long id) {
+        return vaultRepository.getVaultById(id).getData();
     }
 }

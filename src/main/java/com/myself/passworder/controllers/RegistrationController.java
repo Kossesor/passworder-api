bda410a220d7bd97fb3fe45601f79e14db19ebbe
FiNaLiZee <sd103e@mail.ru>
2022-03-12 19:54:26 +0300

    Handle exeptions

--- src/main/java/com/myself/passworder/controllers/RegistrationController.java
+++ src/main/java/com/myself/passworder/controllers/RegistrationController.java
@@ -1,5 +1,7 @@
 package com.myself.passworder.controllers;
 
+import com.myself.passworder.exeptions.UserEmailAlreadyExistException;
+import com.myself.passworder.exeptions.UsernameAlreadyExistException;
 import com.myself.passworder.models.User;
 import com.myself.passworder.services.UserService;
 import lombok.RequiredArgsConstructor;
@@ -13,8 +15,15 @@ public class RegistrationController {
     private final UserService userService;
 
     @PostMapping("/registration")
-    public void addUser(@RequestBody @Valid User user) {
-        System.out.println(user);
+    public int addUser(@RequestBody @Valid User user) {
+        try {
+            userService.addUser(user);
+            return 0;
+        } catch (UserEmailAlreadyExistException e) {
+            return 1;
+        } catch (UsernameAlreadyExistException e) {
+            return 2;
+        }
     }
 
     @GetMapping("/activation/{code}")
